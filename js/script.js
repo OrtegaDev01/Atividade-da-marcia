52a2b408206e0d40c9917137a3e5d7192883d905
Felipe Ortega da Silva <eusoortega@gmail.com>
2025-10-28 09:31:27 -0300

    Vaticanização v3

--- js/script.js
+++ js/script.js
@@ -1,42 +1,9 @@
 let tabela_jogador = document.getElementById("tabela-usuarios");
-const form_user = document.getElementById("form-usuario");
 const overlay = document.getElementById("overlay");
 const caixa_form = document.getElementById("prompt");
 let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
-function cancelar() {
-  overlay.style.display = "none";
-  caixa_form.style.display = "none"
-}
-function deletar(i) {
-  usuarios.splice(i, 1);
-  localStorage.setItem("usuarios", JSON.stringify(usuarios));
-  window.location.reload();
-}
-function mostrar_prompt(i) {
-  overlay.style.display = "block";
-  caixa_form.style.display = "flex";
-  caixa_form.style.flexDirection = "column"
-  caixa_form.style.justifyContent = "space-between";
-  caixa_form.style.alignItems = "center";
-  caixa_form.style.gap = "10px"
-  caixa_form.addEventListener("submit", function(x) {
-    x.preventDefault();
-    let caixa_nome = document.getElementById("nome-user").value;
-    let caixa_email = document.getElementById("email-user").value;
-    if (caixa_nome.trim() != '' && caixa_email.trim() != '') {
-      usuarios[i].nome = caixa_nome;
-      usuarios[i].email = caixa_email;
-      localStorage.setItem("usuarios", JSON.stringify(usuarios))
-      overlay.style.display = "none";
-      caixa_form.style.display = "none";
-    }
-    window.location.reload();
-  })
-
-}
 function exibir_users() {
   document.getElementById("qtde-usuarios").innerText = `Quantidade usuários cadastrados:${usuarios.length}`;
-
   tabela_jogador.innerHTML = "";
   for (let i = 0; i < usuarios.length; i++) {
     let linha = document.createElement("tr");
@@ -46,17 +13,40 @@ function exibir_users() {
     excluir.type = "button";
     excluir.value = "Excluir";
     excluir.classList.add("bt-excluir");
-    excluir.onclick = () => deletar(i);
-    document.getElementsByClassName("td-botoes")[i].appendChild(excluir);
+    excluir.onclick = (i) => {
+      usuarios.splice(i, 1);
+      localStorage.setItem("usuarios", JSON.stringify(usuarios))
+      window.location.reload();
+    }
     let editar = document.createElement("input");
     editar.type = "button";
     editar.value = "Editar";
     editar.classList.add("bt-editar");
-    editar.onclick = () => mostrar_prompt(i);
+    editar.onclick = () => {
+      overlay.style.display = "block";
+      caixa_form.style.display = "flex";
+      caixa_form.style.gap = "13px"
+      caixa_form.style.flexDirection = "column";
+      caixa_form.style.justifyContent = "space-between";
+      caixa_form.style.alignItems = "center";
+      caixa_form.addEventListener("submit", () => {
+        let caixa_nome = document.getElementById("nome-user").value;
+        let caixa_email = document.getElementById("email-user").value;
+        if (caixa_nome.trim() != '' && caixa_email.trim() != '') {
+          usuarios[i].nome = caixa_nome;
+          usuarios[i].email = caixa_email;
+          localStorage.setItem("usuarios", JSON.stringify(usuarios))
+          overlay.style.display = "none";
+          caixa_form.style.display = "none";
+        }
+        window.location.reload();
+      })
+    };
+    document.getElementsByClassName("td-botoes")[i].appendChild(excluir);
     document.getElementsByClassName("td-botoes")[i].appendChild(editar);
   }
 }
-form_user.addEventListener("submit", function(evento) {
+document.getElementById("form-usuario").addEventListener("submit", (evento) => {
   evento.preventDefault();
   let nome = document.getElementById("nome_usuario").value;
   let email = document.getElementById("email_usuario").value;
@@ -64,13 +54,17 @@ form_user.addEventListener("submit", function(evento) {
   localStorage.setItem("usuarios", JSON.stringify(usuarios));
   exibir_users();
 });
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", () => {
   exibir_users();
 });
-function resetar() {
+document.getElementById("vaticano").addEventListener("click", () => {
+  window.location.href = "https://www.vatican.va/content/vatican/pt.html"
+})
+document.getElementById("cancelar").addEventListener("click", () => {
+  overlay.style.display = "none";
+  caixa_form.style.display = "none";
+})
+document.getElementById("bt-reset").addEventListener("click", () => {
   localStorage.clear();
   window.location.reload();
-}
-document.getElementById("vaticano").addEventListener("click", function() {
-  window.location.href = "https://www.vatican.va/content/vatican/pt.html"
 })
